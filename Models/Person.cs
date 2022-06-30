b548b0c4a70c4dc285680d637bfe4b0e9de4e080
roamschemer <roamschemer@gmail.com>
2022-07-01 08:48:16 +0900

    Personクラスは今のところ通知不要なので通知を削除

--- Models/Person.cs
+++ Models/Person.cs
@@ -3,19 +3,9 @@ using MauiTry.Services;
 
 namespace MauiTry.Models
 {
-    public class Person : BindableBase
+    public class Person
     {
-        public string Name
-        {
-            get => _name;
-            set => SetProperty(ref _name, value);
-        }
-        private string _name;
-
-        public string Job {
-            get => _job;
-            set => SetProperty(ref _job, value);
-        }
-        private string _job;
+        public string Name { get; set; }
+        public string Job { get; set; }
     }
 }
